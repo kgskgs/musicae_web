37e8ff4774b36869ba8dfcf12c474c47053e4555
Ubuntu <ubuntu@ip-172-26-4-252.eu-central-1.compute.internal>
2020-10-29 13:50:17 +0000

    language change fix

--- musicae_base/context_processors.py
+++ musicae_base/context_processors.py
@@ -1,5 +1,6 @@
 from .models import UrlItem
-
+import re
+from django.conf import settings
 
 def header_processor(request):
     menuItems = UrlItem.objects.raw(
@@ -7,6 +8,10 @@ def header_processor(request):
         "id NOT IN (SELECT to_urlitem_id FROM musicae_base_urlitem_children) AND showOnBar = 1 "
         "ORDER BY position"
     )
-    context = {'menuItems': menuItems, 'showBanner': True}
+    #lang_next = '/'.join(request.path.split('/')[1:])
+    langs = [fr'\/{x[0]}' for x in settings.LANGUAGES]
+    sub_langs = '|'.join(langs) #request.path
+    lang_next = re.sub(sub_langs, '', request.path)
+    context = {'menuItems': menuItems, 'showBanner': True, "lang_next": lang_next}
 
     return context
