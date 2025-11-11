c224a1ab0a8b5b625d7f51b487db3943555887ed
kakehavata <chris_vassilev@yahoo.com>
2025-11-11 22:55:30 +0100

    DEWWW

--- musicae_web/settings.py
+++ musicae_web/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": os.environ.get("DJANGO_DB_NAME", "musicae_db"),
         "USER": os.environ.get("DJANGO_DB_USER", "django"),
-        "PASSWORD": os.environ.get("DJANGO_DB_PASS", "admin"),
+        "PASSWORD": os.environ.get("DJANGO_DB_PASS", "TheHobbit123!"),
         "HOST": os.environ.get("DJANGO_DB_HOST", "127.0.0.1"),
         "PORT": os.environ.get("DJANGO_DB_PORT", "3306"),
         "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
