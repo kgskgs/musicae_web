f8366832378baa16726068d163047739d3ac4706
kakehavata <chris_vassilev@yahoo.com>
2025-11-14 13:01:04 +0100

    Orcid in Person_detail template

--- musicae_web/sitemaps.py
+++ musicae_web/sitemaps.py
@@ -24,6 +24,7 @@ class ModelObjSitemap(Sitemap):
             Person.objects.all(),
             Publication.objects.all(),
             News.objects.all(),
+            
         ))
         return objs
 
